5187b472aae32ce9f2ea06897e69b46cc08128f1
Ibrahima <ibrah22diallo@gmail.com>
2026-08-03 01:10:50 +0000

    Fix typo pyton -> python

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ set -o errexit
 
 pip install -r requirements.txt
 python manage.py tailwind build
-pyton manage.py tailwind start
+python manage.py tailwind start
 python manage.py collectstatic --no-input
 python manage.py migrate
